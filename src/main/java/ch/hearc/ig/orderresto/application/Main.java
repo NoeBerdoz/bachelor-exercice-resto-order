13627b5b9efbbf1adaf3ff080d609de1c0f7fcd3
NoeBerdoz <berdoznoe@gmail.com>
2024-10-24 22:56:09 +0200

    Played with a logger for the first time, it's cool

--- src/main/java/ch/hearc/ig/orderresto/application/Main.java
+++ src/main/java/ch/hearc/ig/orderresto/application/Main.java
@@ -3,23 +3,31 @@ package ch.hearc.ig.orderresto.application;
 import ch.hearc.ig.orderresto.presentation.MainCLI;
 import ch.hearc.ig.orderresto.utils.OracleConnector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.SQLException;
 
 public class Main {
 
-  public static void main(String[] args) throws SQLException {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    // TODO
-    // Add validation/error handling for PropertiesLoader in case properties are missing or fail to load.
-    // Integrate a proper logging system (SLF4J with a logger implementation like Logback).
-    // Ensure the connection pool is always closed properly during application shutdown.
+    public static void main(String[] args) throws SQLException {
 
-    OracleConnector.isDatabaseConnectable();
-    OracleConnector.getConnectionFromPool();
-    System.out.println("[+] Open Connection from pool!");
-    OracleConnector.closePool();
-    System.out.println("[-] Closed connection from pool!");
+        // TODO
+        // Add validation/error handling for PropertiesLoader in case properties are missing or fail to load.
+        // Integrate a proper logging system (SLF4J with a logger implementation like Logback).
+        // Ensure the connection pool is always closed properly during application shutdown.
+        // I will need to manage the database commits myself i think
 
-    (new MainCLI()).run();
-  }
+        OracleConnector.isDatabaseConnectable();
+
+        OracleConnector.getConnectionFromPool();
+        logger.info("Open Connection from pool!");
+        System.out.println("[+] Open Connection from pool!");
+        OracleConnector.closePool();
+        System.out.println("[-] Closed connection from pool!");
+
+        (new MainCLI()).run();
+    }
 }
--- src/main/java/ch/hearc/ig/orderresto/utils/OracleConnector.java
+++ src/main/java/ch/hearc/ig/orderresto/utils/OracleConnector.java
@@ -2,12 +2,16 @@ package ch.hearc.ig.orderresto.utils;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class OracleConnector {
+    private static final Logger logger = LoggerFactory.getLogger(OracleConnector.class);
+
     private static PropertiesLoader propertiesLoader = new PropertiesLoader("database.properties");
 
     private static final String DB_URL = propertiesLoader.getProperty("db.url");
@@ -16,11 +20,6 @@ public class OracleConnector {
     private static final String DB_POOL_SIZE = propertiesLoader.getProperty("db.pool.size");
 
     private static HikariDataSource dataSource;
-
-    // Static block for automatic initialization
-    static {
-        setPoolConfig();
-    }
 
     public static void setPoolConfig() {
         HikariConfig config = new HikariConfig();
@@ -34,6 +33,9 @@ public class OracleConnector {
     }
 
     public static Connection getConnectionFromPool() throws SQLException {
+        if (dataSource == null) {
+            setPoolConfig();
+        }
         return dataSource.getConnection();
     }
 
@@ -45,15 +47,10 @@ public class OracleConnector {
 
     public static boolean isDatabaseConnectable() throws SQLException {
         try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
-            if (connection != null) {
-                System.out.println("[+] Connection established!");
-                return true;
-            } else {
-                System.out.println("[-] Connection failed!");
-                return false;
-            }
+            logger.info("Connection established!");
+            return true;
         } catch (SQLException e) {
-            System.err.println("[-] An error occurred: " + e.getMessage());
+            logger.error("An error occurred: {}", e.getMessage());
             return false;
         }
     }
